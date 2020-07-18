5a09cb4d48a325bb179aa4cf04bcbaf560da2e1c
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-07-18 22:37:30 +0600

    smith

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $('.up').counterUp({
     time: 2500
 });
 //skill-counter-part End//
+$(document).ready(() => {
+    $('.venobox').venobox(
+        {
+            spinner: 'wandering-cubes',
+            framewidth: '800px',
+            frameheight: '800px',
+            border: '10px',
+            share: [],
+            infinigall: true,
+            closeColor: '#fff',
+            numeratio: true,
+        }
+    );
+});
+
 
 
 
