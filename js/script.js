83f3323cf5e7965ba8c8a9d27aeefda82d462a2b
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-07-19 22:48:58 +0600

    smith

--- js/script.js
+++ js/script.js
@@ -61,6 +61,48 @@ $('.pro-slider').slick({
 });
 
 ///portfolio-part-End////
+$('.feed-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '0',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
+///portfolio-part-End////
 
 
 
