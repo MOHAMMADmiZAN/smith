587967eae928314750243f637b1d11f3bffadb74
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-07-22 07:42:11 +0600

    smith

--- js/script.js
+++ js/script.js
@@ -155,7 +155,7 @@ bdy.scrollspy({
 });
 
 // animation scroll js
-$(' .navbar a').on('click', function () {
+$('a').on('click', function () {
     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
         let target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
